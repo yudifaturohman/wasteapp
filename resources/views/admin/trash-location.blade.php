0c214314e5e7d7f21c12733463d2645b3fb6cce9
DEKSTOP-KV5UBTA <DESKSTOP-KV5UBTA@mail.com>
2023-07-11 09:29:31 +0700

    add modal form for add and edit location

--- resources/views/admin/trash-location.blade.php
+++ resources/views/admin/trash-location.blade.php
@@ -11,6 +11,15 @@ Trash Location
         {{ session('message') }}
     </div>
     @endif
+    <div class="page-header">
+        <h3 class="page-title">
+            <span class="page-title-icon bg-gradient-primary text-white me-2">
+                <i class="mdi mdi-map-marker"></i>
+            </span> @yield('title')
+            <button type="button" data-bs-toggle="modal" data-bs-target="#add" class="btn btn-primary btn-sm mx-2">Add
+                Location</button>
+        </h3>
+    </div>
     <div wire:ignore class="row">
         <div class="col-md-12 grid-margin stretch-card">
             <div class="card">
@@ -53,6 +62,92 @@ Trash Location
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+
+<!-- Add Modal -->
+<div wire:ignore.self class="modal fade" id="add" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog ">
+        <div class="modal-content bg-white">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Location</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form class="forms-sample" wire:submit.prevent='store()'>
+                    <div class="form-group">
+                        <label>Location Name</label>
+                        <input wire:model='location_name' type="text" class="form-control">
+                        @error('location_name')
+                        <span class="text-danger"><strong>{{ $message }}</strong></span>
+                        @enderror
+
+                    </div>
+                    <div class="form-group">
+                        <label>Latitude</label>
+                        <input wire:model='lat' type="text" class="form-control">
+                        @error('lat')
+                        <span class="text-danger"><strong>{{ $message }}</strong></span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label>Longitude</label>
+                        <input wire:model='long' type="text" class="form-control">
+                        @error('long')
+                        <span class="text-danger"><strong>{{ $message }}</strong></span>
+                        @enderror
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Edit Modal -->
+<div wire:ignore.self class="modal fade" id="edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog ">
+        <div class="modal-content bg-white">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Location</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form class="forms-sample" wire:submit.prevent='store()'>
+                    <div class="form-group">
+                        <label>Location Name</label>
+                        <input wire:model='location_name' type="text" class="form-control">
+                        @error('location_name')
+                        <span class="text-danger"><strong>{{ $message }}</strong></span>
+                        @enderror
+
+                    </div>
+                    <div class="form-group">
+                        <label>Latitude</label>
+                        <input wire:model='lat' type="text" class="form-control">
+                        @error('lat')
+                        <span class="text-danger"><strong>{{ $message }}</strong></span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label>Longitude</label>
+                        <input wire:model='long' type="text" class="form-control">
+                        @error('long')
+                        <span class="text-danger"><strong>{{ $message }}</strong></span>
+                        @enderror
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Update</button>
+            </div>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/layouts-admin.blade.php
+++ resources/views/layouts-admin.blade.php
@@ -43,7 +43,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="{{ URL('/') }}">
+              <a class="nav-link" href="{{ route('dashboard') }}">
                 <span class="menu-title">Dashboard</span>
                 <i class="mdi mdi-home menu-icon"></i>
               </a>
