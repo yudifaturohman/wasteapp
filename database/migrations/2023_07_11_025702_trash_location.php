d8ac61e263cdf3bc9c551a75f4e6d569695573d7
Yudi Faturohman <yudifaturohman198@gmail.com>
2023-07-11 10:00:12 +0700

    create model and migration trash location

---
+++ database/migrations/2023_07_11_025702_trash_location.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TrashLocation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('location_name');
+            $table->string('lat');
+            $table->string('long');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
