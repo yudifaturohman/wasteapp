cf75ec234275c88c4d07b0ffba0f4151e2079363
Yudi Faturohman <yudifaturohman198@gmail.com>
2023-07-12 09:05:51 +0700

    create migration for feature report

---
+++ app/Models/Report.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['location_id', 'email', 'information', 'status', 'photo_evidence'];
+}
