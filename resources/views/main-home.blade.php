e7b5ceebd7a17b85f5204ee35de1f1c2ad682e9e
Yudi Faturohman <yudifaturohman198@gmail.com>
2023-07-17 10:40:17 +0700

    add list waste image

--- resources/views/main-home.blade.php
+++ resources/views/main-home.blade.php
@@ -102,56 +102,230 @@
                 </div>
             </div>
         </section><!-- End About Section -->
+        <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
 
-        <!-- ======= Services Section ======= -->
-        <section id="services" class="services section-bg">
-            <div class="container" data-aos="fade-up">
+        <div class="section-title">
+          <h2>Jenis Sampah</h2>
+          <p>Contoh Jenis Sampah</p>
+        </div>
 
-                <div class="section-title">
-                    <p>Jenis Sampah</p>
-                </div>
+        <div class="row" data-aos="fade-up" data-aos-delay="100">
+          <div class="col-lg-12">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">Semua</li>
+              <li data-filter=".filter-general">Umum</li>
+              <li data-filter=".filter-paper">Kertas</li>
+              <li data-filter=".filter-plastic">Plastik</li>
+              <li data-filter=".filter-glass">Kaleng & Kaca</li>
+            </ul>
+          </div>
+        </div>
 
-                <div class="row">
-                    <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                        <div class="icon-box">
-                            <div class="icon"><i class="bx bxl-dribbble"></i></div>
-                            <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-                            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias
-                                excepturi sint occaecati cupiditate</p>
-                        </div>
-                    </div>
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
 
-                    <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
-                        <div class="icon-box">
-                            <div class="icon"><i class="bx bx-file"></i></div>
-                            <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-                            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                                dolore eu fugiat nulla</p>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
-                        <div class="icon-box">
-                            <div class="icon"><i class="bx bx-tachometer"></i></div>
-                            <h4 class="title"><a href="">Magni Dolores</a></h4>
-                            <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                                officia deserunt mollit anim</p>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="400">
-                        <div class="icon-box">
-                            <div class="icon"><i class="bx bx-world"></i></div>
-                            <h4 class="title"><a href="">Nemo Enim</a></h4>
-                            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui
-                                blanditiis praesentium voluptatum</p>
-                        </div>
-                    </div>
-
-                </div>
-
+          <div class="col-lg-4 col-md-6 portfolio-item filter-general">
+            <div class="portfolio-wrap">
+              <img src="https://www.conserve-energy-future.com/wp-content/uploads/2021/07/tissue-paper.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://www.conserve-energy-future.com/wp-content/uploads/2021/07/tissue-paper.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Tisu"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Tisu</h4>
+                <p>Sampah Umum</p>
+              </div>
             </div>
-        </section><!-- End Services Section -->
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-general">
+            <div class="portfolio-wrap">
+              <img src="https://i0.wp.com/barisan.co/wp-content/uploads/2021/06/ashtray-pack-cigarettes_23-2148585878.jpeg?fit=626%2C417&ssl=1" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://i0.wp.com/barisan.co/wp-content/uploads/2021/06/ashtray-pack-cigarettes_23-2148585878.jpeg?fit=626%2C417&ssl=1" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Bungkus dan Puntung Rokok"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Bungkus dan Puntung Rokok</h4>
+                <p>Sampah Umum</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-general">
+            <div class="portfolio-wrap">
+              <img src="https://grist.org/wp-content/uploads/2015/01/styrofoam-garbage1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://grist.org/wp-content/uploads/2015/01/styrofoam-garbage1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Styrofoam"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Styrofoam</h4>
+                <p>Sampah Umum</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-general">
+            <div class="portfolio-wrap">
+              <img src="https://www.universaleco.id/uploads/content/majun.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://www.universaleco.id/uploads/content/majun.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Kain Bekas dan Sejenisnya"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Kain Bekas dan Sejenisnya</h4>
+                <p>Sampah Umum</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-general">
+            <div class="portfolio-wrap">
+              <img src="https://greeneration.org/en/wp-content/uploads/2022/07/Sampah-Masker-Bekas-Pakai-624x351-1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://greeneration.org/en/wp-content/uploads/2022/07/Sampah-Masker-Bekas-Pakai-624x351-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Masker Non Medis"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Masker Non Medis</h4>
+                <p>Sampah Umum</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-general">
+            <div class="portfolio-wrap">
+              <img src="https://thumb.viva.co.id/media/frontend/thumbs3/2018/05/03/5aeaa6211351a-kertas-pembungkus-nasi_665_374.jpeg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://thumb.viva.co.id/media/frontend/thumbs3/2018/05/03/5aeaa6211351a-kertas-pembungkus-nasi_665_374.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Bungkus Nasi"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Bungkus Nasi</h4>
+                <p>Sampah Umum</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-general">
+            <div class="portfolio-wrap">
+              <img src="https://panmomo.com/uploads/images/products/gelpen_ballpoint_todayscolor_01_960.jpeg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://panmomo.com/uploads/images/products/gelpen_ballpoint_todayscolor_01_960.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Ballpoint dan Spidol"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Ballpoint dan Spidol</h4>
+                <p>Sampah Umum</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-paper">
+            <div class="portfolio-wrap">
+              <img src="https://dikemas.com/uploads/2021/07/BISNIS-UKM-2-1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://dikemas.com/uploads/2021/07/BISNIS-UKM-2-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Boks Makanan"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Boks Makanan</h4>
+                <p>Sampah Kertas</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-paper">
+            <div class="portfolio-wrap">
+              <img src="https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/ideafoto/original/8158_5-langkah-mengurangi-sampah-kemasan.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/ideafoto/original/8158_5-langkah-mengurangi-sampah-kemasan.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Kotak Minuman"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Kotak Minuman</h4>
+                <p>Sampah Kertas</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-plastic">
+            <div class="portfolio-wrap">
+              <img src="https://akcdn.detik.net.id/visual/2019/11/21/f0d74245-1ec4-4aa8-b131-de905537c69a_169.jpeg?w=650" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://akcdn.detik.net.id/visual/2019/11/21/f0d74245-1ec4-4aa8-b131-de905537c69a_169.jpeg?w=650" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Botol Plastik"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Botol Plastik</h4>
+                <p>Sampah Plastik</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-plastic">
+            <div class="portfolio-wrap">
+              <img src="https://img.okezone.com/content/2020/07/04/320/2241282/fakta-larangan-kantong-plastik-di-dki-jakarta-ancaman-denda-rp25-juta-d81ROXaCtV.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://img.okezone.com/content/2020/07/04/320/2241282/fakta-larangan-kantong-plastik-di-dki-jakarta-ancaman-denda-rp25-juta-d81ROXaCtV.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Kantong Plastik"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Kantong Plastik</h4>
+                <p>Sampah Plastik</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-plastic">
+            <div class="portfolio-wrap">
+              <img src="https://envira.id/wp-content/uploads/2023/02/WhatsApp-Image-2023-02-13-at-17.05.59.jpeg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://envira.id/wp-content/uploads/2023/02/WhatsApp-Image-2023-02-13-at-17.05.59.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Snack"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Snack</h4>
+                <p>Sampah Plastik</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-glass">
+            <div class="portfolio-wrap">
+              <img src="https://gdb.voanews.com/B55C3C50-1122-4BEC-8843-24F10A4A09CE_w1200_r1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://gdb.voanews.com/B55C3C50-1122-4BEC-8843-24F10A4A09CE_w1200_r1.jpg0" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Kaleng"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Kaleng</h4>
+                <p>Sampah Kaleng & Kaca</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-glass">
+            <div class="portfolio-wrap">
+              <img src="https://siapdarling.id/uploads/infodarling/banner_20221205101726_template_image_detail__(2).png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://siapdarling.id/uploads/infodarling/banner_20221205101726_template_image_detail__(2).png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Kaca"><i class="bi bi-plus"></i></a>
+                <a href="#" title="More Details"><i class="bi bi-link"></i></a>
+              </div>
+              <div class="portfolio-info">
+                <h4>Kaca</h4>
+                <p>Sampah Kaleng & Kaca</p>
+              </div>
+            </div>
+          </div>
+          
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
 
     </main><!-- End #main -->
 
